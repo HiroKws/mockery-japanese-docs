f7c72527c4f17267b828711fb4d07d495d50266c
Hirohisa Kawase <hirokws@gmail.com>
2019-11-22 19:09:06 +0900

    変更済みファイルのインデックススクリプト修正

--- index-updated-markdown-in-conoha2019.sh
+++ index-updated-markdown-in-conoha2019.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-git --no-pager diff --name-status $(cat before-commit.txt) | awk -e '($1=="A" || $1 == "M" || $1=="R100") { if($1=="R100" && $3~/\.md$/) { print $3 } else if ($2 ~ /\.md$/) { print $2 }}' | awk -e 'BEGIN {FS="/"} /\/ja\// {print "cd /home/hiro/searchdouble && php artisan index:mockery", $0, $2, $3, gensub(".md", "", "1", $5)}' | bash
+git --no-pager diff --name-status $(cat before-commit.txt) | awk -e '($1=="A" || $1 == "M" || $1=="R100") { if($1=="R100" && $3~/\.md$/) { print $3 } else if ($2 ~ /\.md$/) { print $2 }}' | awk -e 'BEGIN {FS="/"} /\/ja\// {print "cd /home/hiro/searchdouble && php artisan index:mockery /home/hiro/mockery-japanese-doc/" $0, $2, $3, gensub(".md", "", "1", $5)}' | bash
 
 # 現在のコミットIDを保存する
 git rev-parse HEAD > before-commit.txt
